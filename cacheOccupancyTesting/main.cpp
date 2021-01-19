ab1681dc602e91b454e6ed23425518e2ff524d3c
Patrick <ptrick@udel.edu>
2021-01-19 13:55:46 -0500

    Setup linked list gathering

--- cacheOccupancyTesting/main.cpp
+++ cacheOccupancyTesting/main.cpp
@@ -17,8 +17,8 @@
 
 using namespace std;
 
-int64_t counterVal = 0;
-bool run = true;
+volatile int64_t counterVal = 0;
+volatile bool run = true;
 int64_t *timeVals;
 
 
@@ -29,18 +29,9 @@ void timerFunc() {
     }
 }
 
-
-
-
-int main(int argc, const char * argv[]) {
-       
-    
-    
+void runSizeTest() {
     FILE *myFile = fopen("/Users/patrickcronin/Dropbox/Patrick/udel/WINTER2021/CacheOccupancy/pythonExaminer/test.txt", "w");
     thread countThread(timerFunc);
-    
-    
-
     
     int maxSize = 5001;
     int maxIterations = 500;
@@ -52,41 +43,40 @@ int main(int argc, const char * argv[]) {
     
     for(int size = 1; size <= maxSize; size+=500) {
         
+        numLines=32;
+        doublePointerPage **data = (doublePointerPage**)malloc(sizeof(doublePointerPage *) * size);
+        for (int i = 0; i < size; i++) {
+            data[i] = (doublePointerPage*)valloc(sizeof(doublePointerPage));
+        }
+        
+        //generateRandomBook(data, size);
+        generateStraightBook(data, size);
+        
+        volatile doubleLine *old = data[0]->d[0].next;
+        //prime
+        for(int i = 0; i < 32*size-1; i++) {
+            old = old->next;
+        }
+        
+        /*
+        numLines=64
+        singlePointerPage **data = (singlePointerPage**)malloc(sizeof(doublePointerPage *) * size);
+        for (int i = 0; i < size; i++) {
+            data[i] = (singlePointerPage*)valloc(sizeof(doublePointerPage));
+        }
+        
+        generateRandomBook(data, size);
+        //generateStraightBook(data, size);
+        
+        singleLine *old = data[0]->d[0].next;
+        //prime
+        for(int i = 0; i < 64*size-1; i++) {
+            old = old->next;
+        }*/
+
         
         for(int iteration = 0; iteration < maxIterations; iteration++) {
         
-            
-            numLines=32;
-            doublePointerPage **data = (doublePointerPage**)malloc(sizeof(doublePointerPage *) * size);
-            for (int i = 0; i < size; i++) {
-                data[i] = (doublePointerPage*)valloc(sizeof(doublePointerPage));
-            }
-            
-            //generateRandomBook(data, size);
-            generateStraightBook(data, size);
-            
-            doubleLine *old = data[0]->d[0].next;
-            //prime
-            for(int i = 0; i < 32*size-1; i++) {
-                old = old->next;
-            }
-            
-            /*
-            numLines=64
-            singlePointerPage **data = (singlePointerPage**)malloc(sizeof(doublePointerPage *) * size);
-            for (int i = 0; i < size; i++) {
-                data[i] = (singlePointerPage*)valloc(sizeof(doublePointerPage));
-            }
-            
-            generateRandomBook(data, size);
-            //generateStraightBook(data, size);
-            
-            singleLine *old = data[0]->d[0].next;
-            //prime
-            for(int i = 0; i < 64*size-1; i++) {
-                old = old->next;
-            }*/
-            
             //probe
             int64_t start = counterVal;
             old = data[0]->d[0].next;
@@ -97,19 +87,14 @@ int main(int argc, const char * argv[]) {
             int64_t end = counterVal;
             
             timeVals[timeCount++] = end-start;
-            
-            for (int i = 0; i < size; i++) {
-                free(data[i]);
-            }
-            free(data);
-            
+
             printf("%d %d/%d\n", size, iteration, 1000);
         }
         
-        
-        
-        
-        
+        for (int i = 0; i < size; i++) {
+            free(data[i]);
+        }
+        free(data);
                
     }
     
@@ -121,70 +106,72 @@ int main(int argc, const char * argv[]) {
     fclose(myFile);
     
     countThread.join();
+}
+
+
+
+int main(int argc, const char * argv[]) {
+       
+    
+    //runSizeTest();
     
     
     
     
-    
-    /*
-    if(argc == 2) {
+    bool gather = false;
+    if(argc == 2 && gather) {
+        
+        int numLines=32;
+        int size = 2000;
+        int maxIters = 1500;
+        int timeCount = 0;
+        
+        timeVals = (int64_t*)malloc(maxIters*sizeof(int64_t));
         char buffer[200];
-        sprintf(buffer, "/Users/patrickcronin/Dropbox/Patrick/udel/WINTER2021/CacheOccupancy/pythonExaminer/%s.txt", argv[1]);
+        sprintf(buffer, "/Users/patrickcronin/Dropbox/Patrick/udel/WINTER2021/CacheOccupancy/pythonExaminer/Data/%s.txt", argv[1]);
         FILE *myFile = fopen(buffer, "w");
         thread countThread(timerFunc);
         
-
-        
-        volatile int temp;
-        int size = 4E6;
-        int maxIters = 10000;
-        dummyArr = (int*)malloc(size*sizeof(int));
-        timeVals = (int64_t*)malloc(maxIters*sizeof(int64_t));
-        
-        //prime
-        for(int i = 0; i < size; i+=64) {
-            dummyArr[i] = i;
-            //indexArr[i] = i;
+        doublePointerPage **data = (doublePointerPage**)malloc(sizeof(doublePointerPage *) * size);
+        for (int i = 0; i < size; i++) {
+            data[i] = (doublePointerPage*)valloc(sizeof(doublePointerPage));
         }
         
-        for(int iteration = 0; iteration < maxIters; iteration++) {
+        //generateRandomBook(data, size);
+        generateStraightBook(data, size);
         
-            
+        volatile doubleLine *old = data[0]->d[0].next;
+        //prime
+        for(int i = 0; i < numLines*size-1; i++) {
+            old = old->next;
+        }
+        for(int iteration = 0; iteration < maxIters; iteration++) {
             //probe
             int64_t start = counterVal;
-            
-            for(int i = 0; i < size; i+=64) {
-                //temp = dummyArr[indexArr[i]];
-                temp = dummyArr[i];
+            old = data[0]->d[0].next;
+            for(int i = 0; i < numLines*size-1; i++) {
+                old = old->next;
             }
             int64_t end = counterVal;
+            timeVals[timeCount++] = end-start;
 
-            timeVals[iteration] = end-start;
-            //
-            //printf("%d/%d\n", iteration, 100);
         }
-        free(dummyArr);
-        printf("%d\n\n", size);
-        
-        for(int i = 0; i < maxIters; i++) {
-            fprintf(myFile, "%d %d %lld\n", size, i, timeVals[i]);
+        for (int i = 0; i < size; i++) {
+            free(data[i]);
         }
-        free(timeVals);
-                   
+        free(data);
         
+        run = false;
+        for(int i = 0; i < timeCount; i++) {
+            fprintf(myFile, "%lld\n", timeVals[i]);
+        }
         
         fclose(myFile);
         
-        
-        
-        
-        
-        run = false;
         countThread.join();
     } else {
         printf("provide a file name\n");
     }
-     */
     return 0;
 }
 
